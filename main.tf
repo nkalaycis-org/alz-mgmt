875b77467654f1c3e6ec3dc4a3ddb7c3f6a9ec1c
Neslihan Kalayci <34338225+nkalayci@users.noreply.github.com>
2026-06-08 23:58:27 +0200

    Update module versions to 0.21.0 and 0.10.0 to resolve version constraints

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ locals {
 module "management_resources" {
   # <https://registry.terraform.io/modules/Azure/avm-ptn-alz-management/azurerm/latest>
   source  = "Azure/avm-ptn-alz-management/azurerm"
-  version = "0.9.0"
+  version = "0.10.0"
 
   location                     = var.location
   resource_group_name          = local.management_resource_group_name
@@ -49,7 +49,7 @@ module "management_resources" {
 module "management_groups" {
   # <https://registry.terraform.io/modules/Azure/avm-ptn-alz/azurerm/latest>
   source  = "Azure/avm-ptn-alz/azurerm"
-  version = "0.20.2"
+  version = "0.21.0"
 
   architecture_name  = "alz_custom"
   location           = var.location
